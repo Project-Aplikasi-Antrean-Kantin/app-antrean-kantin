403298d35f801a46fe43ea72690eeab033db05b9
asysyaffak <asysyaffak@gmail.com>
2023-03-25 13:00:47 +0700

    ads and tenants scrollable

--- lib/views/tenant.dart
+++ lib/views/tenant.dart
@@ -26,85 +26,87 @@ class _TenantState extends State<Tenant> {
     return MaterialApp(
       home: Scaffold(
           body: SafeArea(
-            child: Column(
-              children: <Widget> [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 20.0,
-                    right: 20.0,
-                    top: 70.0
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget> [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20.0,
+                      right: 20.0,
+                      top: 70.0
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25.0),
+                      child: Image.asset('assets/images/ads.png'),
+                    ),
                   ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(25.0),
-                    child: Image.asset('assets/images/ads.png'),
-                  ),
-                ),
-                Expanded(
-                  child: FutureBuilder<List<TenantModel>>(
-                    future: futureTenant,
-                    builder: (context, snapshot){
-                      if(snapshot.hasData){
-                        return Container(
-                          margin: const EdgeInsets.all (20.0),
-                          child: GridView.builder(
-                            itemCount: snapshot.data!.length,
-                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                crossAxisSpacing: 20.0
-                            ),
-                            itemBuilder: (context, index) {
-                              return Center(
-                                  child: InkWell(
-                                    onTap: (){
-                                      Navigator.push(context, MaterialPageRoute(builder: (context){
-                                        return ListTenant(url: '${url}/${index+1}',);
-                                      }));
-                                    },
-                                    child: Stack(
-                                      children: <Widget> [
-                                        ClipRRect(
-                                            borderRadius: BorderRadius.circular(25.0),
-                                            child: Image.asset('assets/images/tenant.png')
-                                        ),
-                                        Positioned(
-                                          bottom: 0,
-                                          right: 10,
-                                          child: Container(
-                                            decoration: BoxDecoration(
-                                              color: Colors.black12, // warna background
-                                              borderRadius: BorderRadius.circular(10), // radius border
-                                            ),
-                                            child: Padding(
-                                              padding: const EdgeInsets.only(
-                                                  right: 4.0,
-                                                  left: 4.0
+                  FutureBuilder<List<TenantModel>>(
+                      future: futureTenant,
+                      builder: (context, snapshot){
+                        if(snapshot.hasData){
+                          return Container(
+                            margin: const EdgeInsets.all (20.0),
+                            child: GridView.builder(
+                              shrinkWrap: true,
+                              physics: const ScrollPhysics(),
+                              itemCount: snapshot.data!.length,
+                              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 20.0
+                              ),
+                              itemBuilder: (context, index) {
+                                return Center(
+                                    child: InkWell(
+                                      onTap: (){
+                                        Navigator.push(context, MaterialPageRoute(builder: (context){
+                                          return ListTenant(url: '${url}/${index+1}',);
+                                        }));
+                                      },
+                                      child: Stack(
+                                        children: <Widget> [
+                                          ClipRRect(
+                                              borderRadius: BorderRadius.circular(25.0),
+                                              child: Image.asset('assets/images/tenant.png')
+                                          ),
+                                          Positioned(
+                                            bottom: 0,
+                                            right: 10,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors.black12, // warna background
+                                                borderRadius: BorderRadius.circular(10), // radius border
                                               ),
-                                              child: Text(
-                                                '${snapshot.data![index].name}',
-                                                style: const TextStyle(
-                                                    fontSize: 18.0,
-                                                    fontWeight: FontWeight.bold,
-                                                    fontFamily: 'Oxygen'
+                                              child: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    right: 4.0,
+                                                    left: 4.0
+                                                ),
+                                                child: Text(
+                                                  '${snapshot.data![index].name}',
+                                                  style: const TextStyle(
+                                                      fontSize: 18.0,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontFamily: 'Oxygen'
+                                                  ),
                                                 ),
                                               ),
                                             ),
                                           ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                              );
-                            },
-                          ),
-                        );
-                      }else if(snapshot.hasError){
-                        return Text(snapshot.error.toString());
-                      }
-                      return const Center(child: CircularProgressIndicator(color: Colors.red,));
-                    },
-                  ),
-                ),
-              ],
+                                );
+                              },
+                            ),
+                          );
+                        }else if(snapshot.hasError){
+                          return Text(snapshot.error.toString());
+                        }
+                        return const Center(child: CircularProgressIndicator(color: Colors.red,));
+                      },
+                    ),
+                ],
+              ),
             ),
           ),
       ),
