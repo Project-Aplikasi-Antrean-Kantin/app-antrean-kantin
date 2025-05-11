ccea3596d459521834b9ad89c2d8f5a7531d9af0
Adam <ghozyalfalah02@gmail.com>
2025-05-11 12:10:24 +0700

    feat: new url server

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,8 +1,12 @@
 /// A class containing constants used in the Masbro application.
 abstract class MasbroConstants {
-  /// The base URL for the Masbro API.
-  static const String baseUrl = 'http://165.22.98.55';
-  // static const String baseUrl = 'http://103.24.58.36:8000';
+  /// New base URL
+  static const String baseUrl = 'http://68.183.176.53';
+
+  /// PENS base URL
+
+  /// Old base URL
+  // static const String baseUrl = 'http://165.22.98.55';
 
   /// The full API endpoint URL.
   static const String url = '$baseUrl/api';
